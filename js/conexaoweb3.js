f5674ba849b01aa7bcb16071a40006d4517c9f71
lisocarvalho <43457575+lisocarvalho@users.noreply.github.com>
2018-11-08 20:16:45 -0200

    alterações aula 12

--- js/conexaoweb3.js
+++ js/conexaoweb3.js
@@ -2,22 +2,24 @@ var contaUsuario;
 
 // Verifica a conexão Web3 e a conta do usuario
 function verificaConta() {
+    var statusConexao = document.getElementById("statusConexao");
     // Verifica o status da conexão 
     if (web3 && web3.isConnected()) {
         if (web3.eth.accounts[0] == undefined)  {
             console.info('Não esta conectado ao Metamask');
-            $('#statusConexao').text('Desconectado');  
+            statusConexao.innerHTML = "Desconectado";  
         }  else {
             console.info('Conectado. Qual versão da lib Web3 foi injetado pelo Metamask? ' + web3.version.api);
             contaUsuario = web3.eth.accounts[0];
-            $('#statusConexao').text('Conectado a conta ' + contaUsuario);  
+            statusConexao.innerHTML = 'Conectado a conta ' + contaUsuario;  
         }
     } else {
-        $('#statusConexao').text('Desconectado');
+        statusConexao.innerHTML = 'Desconectado';
     }
 }
 
 window.addEventListener('load', async (event) => {
+    var statusConexao = document.getElementById("statusConexao");
     // Navegadores com novo Metamask    
     if (window.ethereum) {
         window.web3 = new Web3(ethereum);
@@ -28,7 +30,7 @@ window.addEventListener('load', async (event) => {
             // Contas agora estão expostas                  
         } catch (error) { // Usuário ainda não deu permissão para acessar a carteira Ethereum        
             alert('Por favor, dê permissão para acessarmos a sua carteira Ethereum.');
-            $('#statusConexao').text('Desconectado');
+            statusConexao.innerHTML = 'Desconectado';
         }
     } else if (window.web3) { // Navegadores DApp antigos
         window.web3 = new Web3(web3.currentProvider)
