58e4a7b065f8d6afe43995aa49a3d259dcc1fe60
lisocarvalho <43457575+lisocarvalho@users.noreply.github.com>
2018-11-08 20:26:48 -0200

    alteração hash contrato

--- js/usodeimagem.js
+++ js/usodeimagem.js
@@ -58,7 +58,7 @@ const contratoUsoDeImagemABI = [
 	}
 ];
 
-var contratoUsoDeImagem = web3.eth.contract(contratoUsoDeImagemABI).at("0x9d00df94db52803bf0fcdb248dd4db3bb25f0296");
+var contratoUsoDeImagem = web3.eth.contract(contratoUsoDeImagemABI).at("0x70f87d7fe427843ad7304d0fee66818f135743e0");
 
 function obtemNomeEmpresa() {
     contratoUsoDeImagem.nomeEmpresa({from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
